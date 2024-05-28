8d5b5d2afaf73ebae3ff946ea79a5c438389bac4
mac <mac@macs-MacBook-Pro.local>
2024-05-28 16:15:32 +0530

    name is done

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const testimonials = [
     {
         id: 1,
-        name: "Noah",
+        name: "Wesley Ian" ,
         text: "  At RedWolf Web-Tech, the App Development service surpassed my expectations. Their team exhibited unparalleled expertise, delivering a seamless and user-friendly application that perfectly aligned with my vision.From concept to launch, they were attentive to detail and ensured a smooth development process.I highly recommend their services to anyone seeking innovative and high- quality app solutions.",
 
 
     },
     {
         id: 2,
-        name: "Angel",
+        name: "Bennett Brooks",
         text: "RedWolf Web-Tech's Web Development service is exceptional. They transformed my vague ideas into  a stunning website that not only looks impressive but also functions flawlessly. Their team'sprofessionalism and commitment to excellence are evident in every aspect of their work. Thanks to their expertise, my online presence has been greatly enhanced, driving more traffic and boosting my business. I'm incredibly satisfied with the results and wouldn't hesitate to work  with them again",
 
     },
     {
         id: 3,
-        name: "Daniel",
+        name: "Elian Powel",
         text: "I can't speak highly enough of RedWolf Web-Tech's Digital Marketing service. Their strategic approach and creative campaigns have significantly elevated my brand's online presence. From social media management to SEO optimization, they've helped me reach and engage with my target audience effectively. Their team is responsive, knowledgeable, and dedicated to delivering measurable results. If you're looking to enhance your digital marketing efforts, look no further than RedWolf Web-Tech",
 
     },
     {
         id: 4,
-        name: "David",
+        name: "Harshit Banerjee",
         text: "RedWolf Web-Tech's Graphic Design service is top-notch. Their talented designers took my ideas and turned them into visually stunning creations that perfectly captured the essence of my brand. Whether it's logos, branding materials, or promotional assets, they consistently deliver high-quality work that stands out. Their attention to detail and willingness to go above and beyond to meet their clients' needs are truly commendable. I'm thrilled with the results and would recommend their services to anyone in need of professional graphic design solutions.   ", img: "https://randomuser.me/api/portraits/women/50.jpg"
     },
     // {
